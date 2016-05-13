9619efa476bed1287474363731577869f77f9e23
Dudu <drauto@outlook.com.br>
2016-05-13 18:02:34 -0300

    tiny-mce adicionado

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from app import views
 
-$ git clone https://github.com/<your-github-username>/my-first-blog.gitg
-
 urlpatterns = patterns('',
                        #url(r'^(?P<id>[0-9]+)/$', views.article,),
                        url(r'^$', views.MandadoList.as_view(), name='lista_mandado'),
