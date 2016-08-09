0100ecdc5ce92a36378d04e2d8256e1cc5f59af8
Dudu <drauto@outlook.com.br>
2016-08-08 23:08:13 -0300

    atualizacao

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,14 +82,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ofjusapp$default',
+        'USER': 'ofjusapp',
+        'PASSWORD': 'dudu1303',
+        'HOST': 'ofjusapp.mysql.pythonanywhere-services.com',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
